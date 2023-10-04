7b3aba67dddf8553dcf2ce5d5aae52758e4ef5e7
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-04 16:05:37 +0500

    feat: jsonParse хелпер

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,13 +2,14 @@ import { useState, useEffect } from 'react';
 import UserCard from '../widgets/user-card';
 import type { User } from '../entities/user';
 import { initialUserJson } from '../entities/user';
+import { jsonFileParse } from '../shared/libs/helpers/jsonParse';
 import './index.css';
 
 function App() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    setUser(JSON.parse(JSON.stringify(initialUserJson)));
+    setUser(jsonFileParse(initialUserJson));
   }, []);
 
   return <main>{user && <UserCard user={user} />}</main>;
---
+++ src/shared/libs/helpers/jsonParse.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+const jsonFileParse = (json: any) => JSON.parse(JSON.stringify(json));
+
+export { jsonFileParse };
