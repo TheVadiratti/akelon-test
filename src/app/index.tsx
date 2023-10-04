817b642c3672d5e2cb4c0fe568f88d387675ff13
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-04 15:48:29 +0500

    feat: получение и отображение данных

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react';
+import UserCard from '../widgets/user-card';
+import type { User } from '../entities/user';
+import jsonData from './data/initial-user.json';
 import './index.css';
 
 function App() {
-  return <div />;
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    setUser(JSON.parse(JSON.stringify(jsonData)));
+  }, []);
+
+  return <main>{user && <UserCard user={user} />}</main>;
 }
 
 export default App;
