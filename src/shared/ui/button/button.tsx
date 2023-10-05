e1731d95c8d02ec897f65cc154bba9a029da17d5
Vadim Belousov <thevadiratti@yandex.ru>
2023-10-05 10:29:36 +0500

    feat: проп disabled в button

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -5,25 +5,33 @@ import Styles from './button.module.css';
 interface Props {
   type: 'button' | 'submit' | 'reset';
   label: string;
+  disabled?: boolean;
   onClick?: MouseEventHandler<HTMLButtonElement>;
   extraClass?: string;
 }
 
-const Button = memo(({ type, label, onClick, extraClass }: Props) => {
-  const classNames = useMemo(() => {
-    const names = [Styles.button];
+const Button = memo(
+  ({ type, label, disabled = false, onClick, extraClass }: Props) => {
+    const classNames = useMemo(() => {
+      const names = [Styles.button];
 
-    if (extraClass) {
-      names.push(extraClass);
-    }
-    return names;
-  }, [extraClass]);
+      if (extraClass) {
+        names.push(extraClass);
+      }
+      return names;
+    }, [extraClass]);
 
-  return (
-    <button className={classNames.join(' ')} type={type} onClick={onClick}>
-      {label}
-    </button>
-  );
-});
+    return (
+      <button
+        className={classNames.join(' ')}
+        type={type}
+        disabled={disabled}
+        onClick={onClick}
+      >
+        {label}
+      </button>
+    );
+  }
+);
 
 export default Button;
